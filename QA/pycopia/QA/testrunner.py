be78cb0a1702b74def4fd1f07b03885040805882
keith.dart <keith.dart@6c75ef7c-f12d-0410-ab73-e5b4eb01a1fe>
2010-01-14 10:09:22 +0000

    This is actually based on my older LGPL code.

--- QA/pycopia/QA/testrunner.py
+++ QA/pycopia/QA/testrunner.py
@@ -1,22 +1,5 @@
-#!/usr/bin/python2.4
+#!/usr/bin/python2.5
 # vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
-
-# Copyright The Android Open Source Project
-
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at 
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Modified by Keith Dart to conform to Pycopia style.
-# Previously modified from pycopia modules by Keith Dart for Android.
 
 
 """Top level test runner.
@@ -24,9 +7,6 @@
 This module provides the primary test runner for the automation framework.
 
 """
-__author__ = 'keith@dartworks.biz (Keith Dart)'
-__original_author__ = 'dart@google.com (Keith Dart)'
-__original_original_author__ = 'keith@kdart.com (Keith Dart)'
 
 import sys
 import os
